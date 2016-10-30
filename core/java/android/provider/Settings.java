aef5772c45c815ea759b0a5d0ce97dd476c5964c
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2016-10-30 20:04:25 +0100

    [2/2] frameworks: show charging current & voltage on lockscreen
    Thanx to ezio84 for the switch ;)
    
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>
    
    ezio 31/10/16: enabled by default

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3609,6 +3609,12 @@ public final class Settings {
          */
         public static final String DISABLE_DASHBOARD_CONDITIONS = "disable_dashboard_conditions";
 
+          /**
+          * Whether to show the negociated charger current in the lockscreen
+          * @hide
+          */
+         public static final String LOCKSCREEN_CHARGING_CURRENT = "lockscreen_charging_current";
+ 
         /**
          * Network traffic indicator, goes from least to greatest significant bitwise
          * 0 = Display up-stream traffic if set
